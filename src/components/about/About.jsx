18fc2ec3b58cf79c0e0f316d0cdab0b9df016b5f
michael <michaelmadlos@hotmail.com>
2023-12-19 11:00:07 +0100

    Add about section

---
+++ src/components/about/About.jsx
@@ -0,0 +1,13 @@
+import Faq from "./FAQ"
+import Stack from "./Stack"
+
+function About() {
+	return (
+		<section className="about">
+			<Stack />
+			<Faq />
+		</section>
+	)
+}
+
+export default About
