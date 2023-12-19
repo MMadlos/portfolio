3f8137d82d5326e03d451d0b040a9ec9772cb34e
michael <michaelmadlos@hotmail.com>
2023-12-19 11:53:46 +0100

    Add styles to personal section

--- src/components/about/Personal.jsx
+++ src/components/about/Personal.jsx
@@ -11,10 +11,22 @@ function Personal() {
 				<h2>Michael Madlos</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida eleifend urna nec molestie. Cras non sem sodales velit dictum molestie.</p>
 			</div>
-			<div className="social-media">
-				<i className="fa-brands fa-github" />
-				<i className="fa-brands fa-linkedin" />
-				<i className="fa-solid fa-envelope" />
+			<div className="social-media-container">
+				<div className="social-media-icon">
+					<a href="#">
+						<i className="fa-brands fa-github" />
+					</a>
+				</div>
+				<div className="social-media-icon">
+					<a href="#">
+						<i className="fa-brands fa-linkedin" />
+					</a>
+				</div>
+				<div className="social-media-icon">
+					<a href="#">
+						<i className="fa-solid fa-envelope" />
+					</a>
+				</div>
 			</div>
 		</section>
 	)
