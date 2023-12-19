405c3aebeb0e2a2c056fff77c69bff1f4915af51
michael <michaelmadlos@hotmail.com>
2023-12-19 18:58:39 +0100

    Add Project Card component

--- src/components/projects/More.jsx
+++ src/components/projects/More.jsx
@@ -1,71 +1,76 @@
 import IMAGE from "../../assets/avatar_test.jpg"
 
+const content = [
+	{
+		title: "Battleship",
+		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida eleifend urna nec molestie. Cras non sem sodales velit dictum molestie.",
+		src: IMAGE,
+		alt: "",
+		website: "#",
+		github: "#",
+	},
+	{
+		title: "To do list",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rerum repudiandae culpa eum? Enim doloremque fugiat a alias placeat?        Accusantium, libero? Nam placeat mollitia quisquam id debitis reprehenderit quod labore. Nam placeat mollitia quisquam id debitis reprehenderit quod labore.",
+		src: IMAGE,
+		alt: "",
+		website: "#",
+		github: "#",
+	},
+	{
+		title: "Card memory game",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rerum repudiandae culpa eum? Enim doloremque fugiat a alias placeat?        Accusantium, libero? Nam placeat mollitia quisquam id debitis reprehenderit quod labore.",
+		src: IMAGE,
+		alt: "",
+		website: "#",
+		github: "#",
+	},
+]
+
+function ProjectCard({ project }) {
+	const { title, description, src, alt, website, github } = project
+	return (
+		<div className="project">
+			<img
+				src={src}
+				alt={alt}
+			/>
+			<div className="content">
+				<h2>{title}</h2>
+				<p>{description}</p>
+			</div>
+			<div className="buttons">
+				<a
+					href={website}
+					className="button main">
+					Website <i className="fa-solid fa-arrow-up-right-from-square"></i>
+				</a>
+				<a
+					href={github}
+					className="button secondary">
+					Github <i className="fa-solid fa-arrow-up-right-from-square"></i>
+				</a>
+			</div>
+		</div>
+	)
+}
+
 function More() {
 	return (
 		<section className="more-projects">
 			<h1>More projects</h1>
+
 			<div className="projects-container">
-				<div className="project">
-					<img
-						src={IMAGE}
-						alt=""
-					/>
-					<div className="content">
-						<h2>Battleship</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida eleifend urna nec molestie. Cras non sem sodales velit dictum molestie.</p>
-					</div>
-					<div className="buttons">
-						<a className="button main">
-							Website <i className="fa-solid fa-arrow-up-right-from-square"></i>
-						</a>
-						<a className="button secondary">
-							Github <i className="fa-solid fa-arrow-up-right-from-square"></i>
-						</a>
-					</div>
-				</div>
-				<div className="project">
-					<img
-						src={IMAGE}
-						alt=""
-					/>
-					<div className="content">
-						<h2>To do List</h2>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rerum repudiandae culpa eum? Enim doloremque fugiat a alias placeat?
-							Accusantium, libero? Nam placeat mollitia quisquam id debitis reprehenderit quod labore. Nam placeat mollitia quisquam id debitis reprehenderit
-							quod labore.
-						</p>
-					</div>
-					<div className="buttons">
-						<a className="button main">
-							Website <i className="fa-solid fa-arrow-up-right-from-square"></i>
-						</a>
-						<a className="button secondary">
-							Github <i className="fa-solid fa-arrow-up-right-from-square"></i>
-						</a>
-					</div>
-				</div>
-				<div className="project">
-					<img
-						src={IMAGE}
-						alt=""
-					/>
-					<div className="content">
-						<h2>Card memory game</h2>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rerum repudiandae culpa eum? Enim doloremque fugiat a alias placeat?
-							Accusantium, libero? Nam placeat mollitia quisquam id debitis reprehenderit quod labore.
-						</p>
-					</div>
-					<div className="buttons">
-						<a className="button main">
-							Website <i className="fa-solid fa-arrow-up-right-from-square"></i>
-						</a>
-						<a className="button secondary">
-							Github <i className="fa-solid fa-arrow-up-right-from-square"></i>
-						</a>
-					</div>
-				</div>
+				{content.map((content, index) => {
+					return (
+						<ProjectCard
+							key={index}
+							project={content}
+						/>
+					)
+				})}
 			</div>
 		</section>
 	)
