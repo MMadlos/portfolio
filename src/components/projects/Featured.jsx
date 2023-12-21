88a871361fabdf5720539c240ae2cb4ddc3fd35b
michael <michaelmadlos@hotmail.com>
2023-12-21 12:07:06 +0100

    Add image composition for feature project section

--- src/components/projects/Featured.jsx
+++ src/components/projects/Featured.jsx
@@ -1,6 +1,47 @@
 import IMAGE from "../../assets/avatar_test.jpg"
 import Buttons from "./Buttons"
 
+function ImageComposition() {
+	return (
+		<div className="main-container">
+			<div className="left">
+				<img
+					src={IMAGE}
+					alt=""
+				/>
+				<img
+					src={IMAGE}
+					alt=""
+				/>
+			</div>
+			<div className="center">
+				<img
+					src={IMAGE}
+					alt=""
+				/>
+			</div>
+			<div className="right">
+				<div className="top">
+					<img
+						src={IMAGE}
+						alt=""
+					/>
+					<img
+						src={IMAGE}
+						alt=""
+					/>
+				</div>
+				<div className="btm">
+					<img
+						src={IMAGE}
+						alt=""
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
+
 function Featured() {
 	return (
 		<section className="featured-project">
@@ -8,10 +49,7 @@ function Featured() {
 				<h1>Featured project</h1>
 				<h2>OSHOP</h2>
 			</div>
-			<img
-				src={IMAGE}
-				alt=""
-			/>
+			<ImageComposition />
 			<p className="description">
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida eleifend urna nec molestie. Cras non sem sodales velit dictum molestie.
 			</p>
