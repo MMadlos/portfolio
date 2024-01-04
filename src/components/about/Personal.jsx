b607c06e9cbaf51f9f39a15af87cbf1ea4140206
michael <michaelmadlos@hotmail.com>
2024-01-04 16:48:44 +0100

    Add scroll reveal effect

--- src/components/about/Personal.jsx
+++ src/components/about/Personal.jsx
@@ -7,9 +7,16 @@ function Personal() {
 				src={AVATAR}
 				alt="Picture of Michael Madlos"
 			/>
-			<div className="description">
-				<h2>Michael Madlos</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida eleifend urna nec molestie. Cras non sem sodales velit dictum molestie.</p>
+			<div className="description scroll-reveal">
+				<h2>
+					<span>Michael Madlos</span>
+				</h2>
+
+				<p>
+					<span>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida eleifend urna nec molestie. Cras non sem sodales velit dictum molestie.
+					</span>
+				</p>
 			</div>
 			<div className="social-media-container">
 				<div className="social-media-icon">
