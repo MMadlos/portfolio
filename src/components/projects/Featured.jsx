1e149ea8125a7f991afc1b1221d82cf3673152ee
michael <michaelmadlos@hotmail.com>
2024-01-09 17:52:52 +0100

    Add LanguageContext

--- src/components/projects/Featured.jsx
+++ src/components/projects/Featured.jsx
@@ -1,6 +1,9 @@
 import IMAGE from "../../assets/avatar_test.jpg"
 import Buttons from "./Buttons"
 
+import { useContext } from "react"
+import { LanguageContext } from "../../App"
+
 function ImageComposition() {
 	return (
 		<div className="main-container">
@@ -42,17 +45,29 @@ function ImageComposition() {
 	)
 }
 
+const content = {
+	sectionName: {
+		en: "Featured project",
+		es: "Proyecto destacado",
+	},
+	description: {
+		en: "My most complete use of React. It includes React Router, React Testing library and the API is from fakestore.com",
+		es: "Proyecto en el que recopila todo lo aprendido de React. Incluye el uso de React Router, React Testing Library y la API es de fakestore.com.",
+	},
+}
+
 function Featured() {
+	const { contentLang } = useContext(LanguageContext)
+	const { sectionName, description } = content
+
 	return (
 		<section className="featured-project">
 			<div className="title">
-				<h1>Featured project</h1>
+				<h1>{sectionName[contentLang]}</h1>
 				<h2>OSHOP</h2>
 			</div>
 			<ImageComposition />
-			<p className="description">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed gravida eleifend urna nec molestie. Cras non sem sodales velit dictum molestie.
-			</p>
+			<p className="description">{description[contentLang]}</p>
 
 			<Buttons
 				website="https://online-shop-brown-two.vercel.app/"
