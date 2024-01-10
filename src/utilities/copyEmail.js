cc8fe04db8e3ce8117a5afe297bfea4c09794f00
michael <michaelmadlos@hotmail.com>
2024-01-10 16:58:03 +0100

    Refactor copy email and split content from utility function

--- src/utilities/copyEmail.js
+++ src/utilities/copyEmail.js
@@ -1,14 +1,6 @@
-async function copyEmail(contentLang) {
+async function copyEmail() {
 	const email = "michaelmadlos@gmail.com"
 	await navigator.clipboard.writeText(email)
-
-	// TODO -> Animación para indicar que el texto se ha copiado
-	const copyEmailText = {
-		en: "Email copied",
-		es: "Email copiado",
-	}
-
-	alert(copyEmailText[contentLang])
 }
 
 export { copyEmail }
