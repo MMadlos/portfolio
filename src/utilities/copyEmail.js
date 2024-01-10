21e52075e5fab258552edf6e4b2f1b3923493288
michael <michaelmadlos@hotmail.com>
2024-01-10 10:34:01 +0100

    Add parameter to show message in the language selected

--- src/utilities/copyEmail.js
+++ src/utilities/copyEmail.js
@@ -1,10 +1,14 @@
-async function copyEmail() {
+async function copyEmail(contentLang) {
 	const email = "michaelmadlos@gmail.com"
-
 	await navigator.clipboard.writeText(email)
 
 	// TODO -> Animación para indicar que el texto se ha copiado
-	alert("Email copied")
+	const copyEmailText = {
+		en: "Email copied",
+		es: "Email copiado",
+	}
+
+	alert(copyEmailText[contentLang])
 }
 
 export { copyEmail }
