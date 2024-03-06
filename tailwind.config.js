f5b067a7c0337976756cdd2c9cfc820dd1a7fe05
MMadlos <michaelmadlos@gmail.com>
2024-03-06 11:57:50 +0100

    Add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      fira: ["FiraCode", ...defaultTheme.fontFamily.sans],
+      inter: ["Inter", ...defaultTheme.fontFamily.sans],
+      geist: ["Geist", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
