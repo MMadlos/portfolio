798cf192c8e55313b599c8574d3e080c05a1a6e8
MMadlos <michaelmadlos@gmail.com>
2024-03-06 11:59:26 +0100

    Remove previous code

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
 import "./App.css";
-import { useState, createContext } from "react";
 
-import Header from "./components/header/Header";
-import Projects from "./components/projects/Projects";
 import About from "./components/about/About";
-import Footer from "./components/footer/Footer";
-
-export const LanguageContext = createContext({
-  lang: "",
-  switchedLang: "",
-  contentLang: "",
-  setLang: () => {},
-  handleLang: () => {},
-});
 
 function App() {
-  const browserLang = navigator.language;
-  const langTag = browserLang.includes("es") ? "Castellano" : "English";
-
-  const [lang, setLang] = useState(langTag);
-
-  const contentLang = lang === "Castellano" ? "es" : "en";
-  const switchedLang = lang === "Castellano" ? "English" : "Castellano";
-  const handleLang = () => setLang(switchedLang);
-
-  return (
-    <LanguageContext.Provider
-      value={{ lang, switchedLang, contentLang, setLang, handleLang }}
-    >
-      <Header />
-      <Projects />
-      <About />
-      <Footer />
-    </LanguageContext.Provider>
-  );
+  return <About />;
 }
 
 export default App;
