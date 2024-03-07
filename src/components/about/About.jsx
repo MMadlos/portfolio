51c4fad7fbdca24a5bdbc41167267f653422d70d
MMadlos <michaelmadlos@gmail.com>
2024-03-07 12:52:51 +0100

    Add tags

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -33,14 +33,19 @@ export default function About() {
             eleifend urna nec molestie. Cras non sem sodales velit dictum
             molestie.
           </p>
+
+          <Stack />
+
           <div className="hidden sm:flex-row gap-4 sm:flex">
             <button className="p-[2px] bg-gradient-to-r from-[#3604FF] via-[#FF4D86] to-[#FDB437] rounded-lg group">
               <span className="bg-black block py-3 text-sm rounded-lg px-6 font-fira text-[#FAFAFA] font-semibold group-hover:bg-gradient-to-r from-[#3604FF] via-[#FF4D86] to-[#FDB437]">
                 LinkedIn
+                <i className="fa-solid fa-arrow-right -rotate-45 ml-2" />
               </span>
             </button>
             <button className="font-fira font-semibold text-[#FAFAFA] rounded-lg text-sm py-3 bg-[#222222] px-6 hover:bg-[#333]">
               Github
+              <i className="fa-solid fa-arrow-right -rotate-45 ml-2" />
             </button>
           </div>
         </div>
@@ -57,5 +62,46 @@ export default function About() {
         </div>
       </div>
     </section>
+  );
+}
+
+function Stack() {
+  const stackContent = [
+    { name: "Javascript", src: "src/assets/logos/logo_js.png" },
+    { name: "React", src: "src/assets/logos/logo_react.png" },
+    { name: "HTML", src: "src/assets/logos/logo_html.png" },
+    { name: "CSS", src: "src/assets/logos/logo_css.png" },
+    { name: "React Router", src: "src/assets/logos/logo_react_router.png" },
+    { name: "Git", src: "src/assets/logos/logo_git.png" },
+    { name: "Github", src: "src/assets/logos/logo_github.png" },
+    { name: "Vite", src: "src/assets/logos/logo_vite.png" },
+    { name: "Webpack", src: "src/assets/logos/logo_webpack.png" },
+    { name: "Vitest", src: "src/assets/logos/logo_vitest.png" },
+    { name: "Jest", src: "src/assets/logos/logo_jest.png" },
+    { name: "React Testing Library", src: "src/assets/logos/logo_RTL.png" },
+    { name: "Figma", src: "src/assets/logos/logo_figma.png" },
+  ];
+
+  return (
+    <div className="flex flex-row gap-2 flex-wrap">
+      {stackContent.map((tag, index) => {
+        const { name, src } = tag;
+
+        return <Tag key={index} text={name} srcLogo={src} />;
+      })}
+    </div>
+  );
+}
+
+function Tag({ text = "", srcLogo }) {
+  return (
+    <div className="bg-[#222222] w-fit rounded-full px-4 py-2 flex flex-row gap-2 justify-center items-center">
+      <img
+        src={srcLogo}
+        alt=""
+        className="size-4 aspect-square object-contain"
+      />
+      <p className="text-[#aaaaaa] font-medium text-xs font-geist">{text}</p>
+    </div>
   );
 }
