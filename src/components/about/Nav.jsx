6e01ea37a025bcc6078ba5a85ce92391146dae06
MMadlos <michaelmadlos@gmail.com>
2024-03-07 18:24:58 +0100

    Replace links with Link component

--- src/components/about/Nav.jsx
+++ src/components/about/Nav.jsx
@@ -1,20 +1,10 @@
+import Link from "../Link/Link";
+
 export default function Nav() {
   return (
     <nav className="flex flex-row gap-8 justify-between sm:justify-normal sm:items-center">
-      <a href="#" className="group">
-        <div className="bg-black h-full py-2 text-sm font-fira text-[#FAFAFA] sm:text-[#999] font-semibold group-hover:text-[#FAFAFA]">
-          <span>Linkedin</span>
-          <i className="fa-solid fa-arrow-right -rotate-45 ml-2" />
-        </div>
-        <div className="h-[2px] w-full bg-gradient-to-r from-[#3604FF] via-[#FF4D86] to-[#FDB437] rounded-full opacity-0 group-hover:opacity-100"></div>
-      </a>
-      <a href="#" className="group">
-        <div className="bg-black h-full py-2 text-sm font-fira text-[#FAFAFA] sm:text-[#999] font-semibold group-hover:text-[#FAFAFA]">
-          <span>Github</span>
-          <i className="fa-solid fa-arrow-right -rotate-45 ml-2" />
-        </div>
-        <div className="h-[2px] w-full bg-gradient-to-r from-[#3604FF] via-[#FF4D86] to-[#FDB437] rounded-full opacity-0 group-hover:opacity-100"></div>
-      </a>
+      <Link text="LinkedIn" />
+      <Link text="Github" />
     </nav>
   );
 }
