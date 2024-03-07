0371df1a2f6c449b1e2d8488675aa634a8523c6c
MMadlos <michaelmadlos@gmail.com>
2024-03-07 16:56:45 +0100

    Add content file with stacklabels

--- src/components/about/Stack.jsx
+++ src/components/about/Stack.jsx
@@ -1,23 +1,9 @@
-export default function Stack() {
-  const stackContent = [
-    { name: "Javascript", src: "src/assets/logos/logo_js.png" },
-    { name: "React", src: "src/assets/logos/logo_react.png" },
-    { name: "HTML", src: "src/assets/logos/logo_html.png" },
-    { name: "CSS", src: "src/assets/logos/logo_css.png" },
-    { name: "React Router", src: "src/assets/logos/logo_react_router.png" },
-    { name: "Git", src: "src/assets/logos/logo_git.png" },
-    { name: "Github", src: "src/assets/logos/logo_github.png" },
-    { name: "Vite", src: "src/assets/logos/logo_vite.png" },
-    { name: "Webpack", src: "src/assets/logos/logo_webpack.png" },
-    { name: "Vitest", src: "src/assets/logos/logo_vitest.png" },
-    { name: "Jest", src: "src/assets/logos/logo_jest.png" },
-    { name: "React Testing Library", src: "src/assets/logos/logo_RTL.png" },
-    { name: "Figma", src: "src/assets/logos/logo_figma.png" },
-  ];
+import { stackLabels } from "../../content/stackLabels";
 
+export default function Stack() {
   return (
     <div className="flex flex-row gap-2 flex-wrap">
-      {stackContent.map((tag, index) => {
+      {stackLabels.map((tag, index) => {
         const { name, src } = tag;
 
         return <Tag key={index} text={name} srcLogo={src} />;
---
+++ src/content/stackLabels.js
@@ -0,0 +1,15 @@
+export const stackLabels = [
+  { name: "Javascript", src: "src/assets/logos/logo_js.png" },
+  { name: "React", src: "src/assets/logos/logo_react.png" },
+  { name: "HTML", src: "src/assets/logos/logo_html.png" },
+  { name: "CSS", src: "src/assets/logos/logo_css.png" },
+  { name: "React Router", src: "src/assets/logos/logo_react_router.png" },
+  { name: "Git", src: "src/assets/logos/logo_git.png" },
+  { name: "Github", src: "src/assets/logos/logo_github.png" },
+  { name: "Vite", src: "src/assets/logos/logo_vite.png" },
+  { name: "Webpack", src: "src/assets/logos/logo_webpack.png" },
+  { name: "Vitest", src: "src/assets/logos/logo_vitest.png" },
+  { name: "Jest", src: "src/assets/logos/logo_jest.png" },
+  { name: "React Testing Library", src: "src/assets/logos/logo_RTL.png" },
+  { name: "Figma", src: "src/assets/logos/logo_figma.png" },
+];
