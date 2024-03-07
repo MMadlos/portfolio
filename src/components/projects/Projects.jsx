10346c97abebcfc9bfe8e20b03b424f0a3dcb3aa
MMadlos <michaelmadlos@gmail.com>
2024-03-07 20:42:17 +0100

    Add styles for mobile screen

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,4 +1,5 @@
 import IMGTest from "../../assets/avatar_test.jpg";
+import Link from "../Link/Link";
 
 export default function Projects() {
   return (
@@ -10,7 +11,9 @@ export default function Projects() {
 
         <div className="flex flex-col gap-20 py-10 ">
           <ProjectInfo name="Cronos" />
+          <Divider />
           <ProjectInfo name="New long name project" />
+          <Divider />
           <ProjectInfo name="Oshop" />
         </div>
       </section>
@@ -18,41 +21,46 @@ export default function Projects() {
   );
 }
 
-function ProjectInfo({ name = "", src = IMGTest }) {
-  return (
-    <article className="font-geist flex flex-col gap-8 sm:flex-row sm:gap-14">
-      <div className="flex flex-col gap-2 sm:max-w-[30%] sm:gap-8">
-        <div>
-          <h3 className="text-lg font-bold ">{name}</h3>
-          <p className="text-[#999999] mt-2 ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-            quae ducimus soluta expedita eos! Nemo quod tenetur voluptatibus
-            cum. Modi quas quam quidem iste itaque laborum minus quasi.
-          </p>
-        </div>
-        <div className="hidden sm:flex sm:flex-row sm:gap-6 sm:w-full font-fira text-sm font-medium ">
-          <button className="group flex flex-row gap-2 items-center h-fit hover:bg-gradient-to-r hover:from-[#3604FF] hover:via-[#FF4D86] hover:to-[#FDB437] hover:text-transparent hover:bg-clip-text text-xs">
-            Website
-            <i className="fa-solid fa-arrow-right -rotate-45 group-hover:text-[#FDB437]" />
-          </button>
-          <button className="group flex flex-row gap-2 items-center h-fit hover:bg-gradient-to-r hover:from-[#3604FF] hover:via-[#FF4D86] hover:to-[#FDB437] hover:text-transparent hover:bg-clip-text text-xs">
-            Github
-            <i className="fa-solid fa-arrow-right -rotate-45 group-hover:text-[#FDB437]" />
-          </button>
-        </div>
-      </div>
-      <div className="bg-white w-full rounded-2xl shadow-lg overflow-hidden sm:max-h-[60vh] sm:shadow-md sm:rounded-md">
-        <img src={src} alt="" className="w-full h-full object-cover" />
-      </div>
+function Divider() {
+  return <div className="bg-[#CACACA] h-[1px] w-full sm:hidden"></div>;
+}
 
-      <div className="flex flex-row gap-4 w-full font-fira text-sm font-semibold sm:hidden">
-        <button className="py-3 px-6 w-full text-white bg-black rounded-xl ">
-          Website
-        </button>
-        <button className="py-3 px-6 w-full text-black rounded-xl border-2 border-black">
-          Github
-        </button>
+function ProjectInfo({
+  name = "",
+  srcIMG = IMGTest,
+  hrefWeb = "#",
+  hrefGithub = "#",
+}) {
+  return (
+    <article className="font-geist flex flex-col gap-6 sm:flex-row sm:gap-14">
+      <div>
+        <h3 className="text-lg font-semibold ">{name}</h3>
+        <div className="hidden sm:flex sm:flex-col gap-6 sm:mt-6">
+          <ProjectDescription />
+        </div>
+      </div>
+      <div className="bg-white w-full rounded-md shadow-md overflow-hidden sm:max-h-[60vh] ">
+        <img src={srcIMG} alt="" className="w-full object-cover aspect-auto" />
+      </div>
+      <div className="sm:hidden flex flex-col gap-6">
+        <ProjectDescription />
       </div>
     </article>
+  );
+}
+
+function ProjectDescription() {
+  return (
+    <>
+      <p className="text-[#999] font-geist text-sm tracking-wide ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quae
+        ducimus soluta expedita eos! Nemo quod tenetur voluptatibus cum. Modi
+        quas quam quidem iste itaque laborum minus quasi.
+      </p>
+      <div className="flex flex-row gap-6">
+        <Link text="Website" light />
+        <Link text="Github" light />
+      </div>
+    </>
   );
 }
