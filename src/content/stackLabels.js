18bc189c926c357389baf8f9460767381846d60f
MMadlos <michaelmadlos@gmail.com>
2024-03-08 11:40:32 +0100

    Replace src with import const

--- src/content/stackLabels.js
+++ src/content/stackLabels.js
@@ -1,15 +1,29 @@
+import JS_LOGO from "../assets/logos/logo_js.png";
+import REACT_LOGO from "../assets/logos/logo_react.png";
+import HTML_LOGO from "../assets/logos/logo_html.png";
+import CSS_LOGO from "src/assets/logos/logo_css.png";
+import RR_LOGO from "src/assets/logos/logo_react_router.png";
+import GIT_LOGO from "src/assets/logos/logo_git.png";
+import GITHUB_LOGO from "src/assets/logos/logo_github.png";
+import VITE_LOGO from "src/assets/logos/logo_vite.png";
+import WEBPACK_LOGO from "src/assets/logos/logo_webpack.png";
+import VITEST_LOGO from "src/assets/logos/logo_vitest.png";
+import JEST_LOGO from "src/assets/logos/logo_jest.png";
+import RTL_LOGO from "src/assets/logos/logo_RTL.png";
+import FIGMA_LOGO from "src/assets/logos/logo_figma.png";
+
 export const stackLabels = [
-  { name: "Javascript", src: "src/assets/logos/logo_js.png" },
-  { name: "React", src: "src/assets/logos/logo_react.png" },
-  { name: "HTML", src: "src/assets/logos/logo_html.png" },
-  { name: "CSS", src: "src/assets/logos/logo_css.png" },
-  { name: "React Router", src: "src/assets/logos/logo_react_router.png" },
-  { name: "Git", src: "src/assets/logos/logo_git.png" },
-  { name: "GitHub", src: "src/assets/logos/logo_github.png" },
-  { name: "Vite", src: "src/assets/logos/logo_vite.png" },
-  { name: "Webpack", src: "src/assets/logos/logo_webpack.png" },
-  { name: "Vitest", src: "src/assets/logos/logo_vitest.png" },
-  { name: "Jest", src: "src/assets/logos/logo_jest.png" },
-  { name: "React Testing Library", src: "src/assets/logos/logo_RTL.png" },
-  { name: "Figma", src: "src/assets/logos/logo_figma.png" },
+  { name: "Javascript", src: JS_LOGO },
+  { name: "React", src: REACT_LOGO },
+  { name: "HTML", src: HTML_LOGO },
+  { name: "CSS", src: CSS_LOGO },
+  { name: "React Router", src: RR_LOGO },
+  { name: "Git", src: GIT_LOGO },
+  { name: "GitHub", src: GITHUB_LOGO },
+  { name: "Vite", src: VITE_LOGO },
+  { name: "Webpack", src: WEBPACK_LOGO },
+  { name: "Vitest", src: VITEST_LOGO },
+  { name: "Jest", src: JEST_LOGO },
+  { name: "React Testing Library", src: RTL_LOGO },
+  { name: "Figma", src: FIGMA_LOGO },
 ];
