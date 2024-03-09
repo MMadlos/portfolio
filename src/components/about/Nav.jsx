0a3faf279c83d264762d0c7014ddf7ba1573be98
MMadlos <michaelmadlos@gmail.com>
2024-03-09 13:17:17 +0100

    add linkedi and github links

--- src/components/about/Nav.jsx
+++ src/components/about/Nav.jsx
@@ -3,8 +3,8 @@ import Link from "../Link/Link";
 export default function Nav() {
   return (
     <nav className="flex flex-row gap-8 justify-between sm:justify-normal sm:items-center">
-      <Link text="LinkedIn" />
-      <Link text="Github" />
+      <Link text="LinkedIn" href="https://es.linkedin.com/in/michaelmadlos" />
+      <Link text="Github" href="https://github.com/MMadlos" />
     </nav>
   );
 }
