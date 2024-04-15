80c812f83d716cedb732db36fc64e0178d6947bf
MMadlos <michaelmadlos@gmail.com>
2024-04-15 20:41:51 +0200

    Add links and styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,20 +2,20 @@ export default function Header() {
   return (
     <header className="w-full">
       <div className="p-4 flex justify-between items-center  bg-[#FAFAFA] ">
-        <h1 className="text-xl uppercase tracking-tighter font-semibold text-stone-900  ">
+        <h1 className="text-xl  tracking-tight font-medium text-stone-900  ">
           Michael Madlos
         </h1>
 
-        <div className="flex  gap-12 text-stone-900 ">
+        <div className="flex gap-12 text-stone-900 ">
           <a
-            href="#"
-            className="flex items-center gap-2 uppercase font-bold hover:text-stone-400"
+            href="https://www.linkedin.com/in/michaelmadlos/"
+            className="flex items-center gap-2 font-medium hover:text-stone-400"
           >
             Linkedin
           </a>
           <a
-            href="#"
-            className="flex items-center gap-2 uppercase  font-bold hover:text-stone-400"
+            href="https://github.com/MMadlos"
+            className="flex items-center gap-2 font-medium hover:text-stone-400"
           >
             Github
           </a>
