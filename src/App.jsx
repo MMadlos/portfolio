b8f450dac8c8b726ed1f6da486965c53b59cc2c8
MMadlos <michaelmadlos@gmail.com>
2024-04-15 12:42:01 +0200

    Move former profject section to archived

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 
 import About from "./components/about/About";
-import Projects from "./components/projects/Projects";
+import Projects from "./components/Z_Archived/Projects";
 
 function App() {
   return (
     <div className="sm:flex sm:flex-row sm:h-dvh sm:overflow-y-hidden">
-      <About />
+      {/* <About /> */}
       <Projects />
     </div>
   );
