e47bb775c88e08f9459f30b794a634b69567a4eb
MMadlos <michaelmadlos@gmail.com>
2024-04-17 16:56:55 +0200

    Fix / Add fixed height to avoid height variations when appearing elements from browser

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <section className="h-[70dvh] flex items-end pb-4">
+    <section className="h-[70vh] flex items-end ">
       <div className="p-4 ">
         <h2 className="tracking-tighter text-3xl sm:text-3xl md:text-4xl lg:text-5xl text-stone-400 font-light xl:max-w-[65vw] ">
           I'm a Front-End Developer aiming to fill the gap between {""}
