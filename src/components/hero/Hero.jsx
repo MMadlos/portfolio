8fa9e1a621a331f77e1607d7644a550ccf1d6148
MMadlos <michaelmadlos@gmail.com>
2024-04-18 12:03:15 +0200

    Add styles and content

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,13 +1,28 @@
 export default function Hero() {
   return (
-    <section className="h-[70vh] flex items-end ">
-      <div className="p-4 overflow-hidden">
-        <h2 className="tracking-tighter text-xl sm:text-3xl md:text-4xl lg:text-5xl text-[#333]/50 font-normal xl:max-w-[65vw] uppercase leading-5">
-          I'm a Front-End Developer aiming to fill the gap between {""}
-          <span className="animate-breathe bg-gradient-to-tl  md:text-nowrap text-[#333] ">
-            development and design.
-          </span>
-        </h2>
+    <section className="h-[60vh] flex items-end p-4">
+      <div className="flex flex-col gap-4">
+        <div className="sm:hidden">
+          <h3 className="text-2xl font-semibold tracking-tighter">
+            Michael Madlos
+          </h3>
+          <div className="flex gap-1 items-center animate-pulse">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              className="size-2  fill-emerald-500"
+            >
+              <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
+            </svg>
+            <span className="text-xs font-medium text-emerald-500">
+              Available
+            </span>
+          </div>
+        </div>
+        <p className="tracking-tighter text-lg sm:text-3xl md:text-4xl lg:text-5xl text-[#333]/60  font-light xl:max-w-[40ch] leading-6 sm:leading-7">
+          I’m a Front-End Developer from Barcelona aiming to deliver exceptional
+          digital experience for the web.
+        </p>
       </div>
     </section>
   );
