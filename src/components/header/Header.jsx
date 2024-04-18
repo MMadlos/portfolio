1d47a6675e4b09f86b6219f3287ba01de956f398
MMadlos <michaelmadlos@gmail.com>
2024-04-18 12:35:39 +0200

    Add styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <header className="w-full fixed top-0 left-0 z-50 ">
-      <div className="py-2 px-4 sm:p-4 flex gap-8 items-center  bg-gradient-to-br from-[#6EE7B7]/50 to-[#A7F1D4]/50 backdrop-blur mx-auto justify-between ">
-        <h1 className="hidden sm:block text-2xl tracking-tighter font-medium pl-2 text-black ">
+      <div className="py-2 px-4 sm:px-4 sm:py-1 flex gap-8 items-center  bg-gradient-to-br from-[#6EE7B7]/50 to-[#A7F1D4]/50 backdrop-blur mx-auto justify-between ">
+        <h1 className="hidden sm:block text-2xl tracking-tighter font-bold pl-2 text-black ">
           Michael Madlos
         </h1>
 
@@ -10,16 +10,16 @@ export default function Header() {
           MM
         </h1>
 
-        <div className="flex gap-4 sm:gap-10">
+        <div className="flex gap-4 ">
           <a
             href="https://www.linkedin.com/in/michaelmadlos/"
-            className="flex items-center font-semibold text-sm sm:text-base sm:font-medium sm:px-4 sm:py-2 hover:text-emerald-500"
+            className="flex items-center font-semibold text-sm sm:font-medium sm:px-4 sm:py-2 hover:text-emerald-500"
           >
             LinkedIn
           </a>
           <a
             href="https://github.com/MMadlos"
-            className="flex items-center font-semibold text-sm sm:text-base sm:font-medium sm:px-4 sm:py-2 hover:text-emerald-500"
+            className="flex items-center font-semibold text-sm sm:font-medium sm:px-4 sm:py-2 hover:text-emerald-500"
           >
             GitHub
           </a>
