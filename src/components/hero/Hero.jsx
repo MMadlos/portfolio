a074198d4689ac890746c9527dda5cb62ea5216a
MMadlos <michaelmadlos@gmail.com>
2024-04-19 13:45:08 +0200

    Add styles

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,11 @@
 export default function Hero() {
   return (
     <section className="h-[60vh]  relative noise-color  ">
-      <div className="flex flex-col justify-end gap-2 px-4 pb-8 md:gap-0 w-full h-full noise">
+      <div className="flex flex-col justify-end gap-4 px-4 pb-8  w-full h-full noise">
         <h3 className="text-3xl font-semibold tracking-tighter text-[#333] md:text-4xl lg:text-5xl">
-          Front-End Developer.
+          Front-End Developer
         </h3>
-        <p className="tracking-tight text-2xl sm:text-3xl md:text-2xl lg:text-3xl text-[#333] leading-7 sm:leading-7 xl:max-w-[80vw]">
+        <p className="tracking-tight text-lg leading-6   sm:text-2xl sm:leading-7 sm:max-w-[30ch]  md:text-2xl lg:text-3xl text-[#333]  md:max-w-[50vw] lg:max-w-[50ch] 2xl:max-w-[50ch]">
           My journey through UX/UI design and customer-centric roles has
           sharpened my analytical and problem-solving skills
         </p>
