b6e57fe26dbecf6d553c9f83974c65f533e74174
MMadlos <michaelmadlos@gmail.com>
2024-05-06 16:32:45 +0200

    Add styles

--- src/components/projects/ProjectList.jsx
+++ src/components/projects/ProjectList.jsx
@@ -2,11 +2,11 @@ import DASHBOARD_IMG from "../../assets/projects/Dashboard.png";
 
 export default function ProjectList() {
   return (
-    <main className="h-dvh bg-[#0E0E0E] px-12  py-12">
+    <main className="bg-[#0E0E0E] px-12  py-12">
       <div className="container mx-auto max-w-[1000px]">
         <h1 className="text-xl font-bold text-white">Projects</h1>
 
-        <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-4 group/list">
           <ProjectCard />
           <ProjectCard />
           <ProjectCard />
@@ -18,7 +18,7 @@ export default function ProjectList() {
 
 function ProjectCard() {
   return (
-    <article className="flex flex-col sm:flex-row gap-16 border border-[#5810A2]/40 py-[24px] px-[40px] rounded-md bg-[#1D0D35]/50 backdrop-blur ">
+    <article className="flex flex-col sm:flex-row gap-16 border border-[#5810A2]/40 py-[24px] px-[40px] rounded-md bg-[#1D0D35]/50 backdrop-blur group-hover/list:opacity-15 hover:!opacity-100 transition-opacity">
       <picture>
         <img
           src={DASHBOARD_IMG}
@@ -28,8 +28,16 @@ function ProjectCard() {
       </picture>
 
       <div className="flex flex-col gap-10">
+        <ul className="flex gap-3 flex-wrap text-[#B080FF] text-xs">
+          <li className="px-4 py-1 bg-[#450B7D]/50 rounded-full ">HTML</li>
+          <li className="px-4 py-1 bg-[#450B7D]/50 rounded-full ">CSS</li>
+          <li className="px-4 py-1 bg-[#450B7D]/50 rounded-full ">
+            JavaScript
+          </li>
+        </ul>
         <div className="flex flex-col gap-2">
           <h2 className="font-bold text-white">Interactive dashboard </h2>
+
           <p className="font-medium text-sm text-white/70">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis
             perferendis magnam obcaecati molestias sunt eligendi optio iste
@@ -38,13 +46,6 @@ function ProjectCard() {
           </p>
         </div>
 
-        <div className="flex gap-3 flex-wrap text-[#B080FF] text-xs">
-          <div className="px-4 py-1 bg-[#450B7D]/50 rounded-full ">HTML</div>
-          <div className="px-4 py-1 bg-[#450B7D]/50 rounded-full ">CSS</div>
-          <div className="px-4 py-1 bg-[#450B7D]/50 rounded-full ">
-            JavaScript
-          </div>
-        </div>
         <div className="flex flex-row gap-4 text-white">
           <a
             href="/"
