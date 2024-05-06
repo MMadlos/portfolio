a3e11d501d8f22c59eeb7109cb45c43b65fb23a5
MMadlos <michaelmadlos@gmail.com>
2024-05-06 12:41:18 +0200

    Add styles

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,14 +1,33 @@
 export default function Hero() {
   return (
-    <section className="h-[60vh]  relative noise-color  ">
-      <div className="flex flex-col justify-end gap-4 px-4 pb-8  w-full h-full noise">
-        <h3 className="text-3xl font-semibold tracking-tighter text-[#333] md:text-4xl lg:text-5xl">
-          Front-End Developer
-        </h3>
-        <p className="tracking-tight text-lg leading-6   sm:text-2xl sm:leading-7 sm:max-w-[30ch]  md:text-2xl lg:text-3xl text-[#333]  md:max-w-[50vw] lg:max-w-[50ch] 2xl:max-w-[50ch]">
-          My journey through UX/UI design and customer-centric roles has
-          sharpened my analytical and problem-solving skills
-        </p>
+    <section className="h-dvh bg-gradient-to-b from-[#305EDF] via-[#8214EF] via-30% to-[#0E0E0E] to-80%">
+      <div class="circle-sm left"></div>
+      <div class="circle-sm right"></div>
+      <div class="circle-lg"></div>
+
+      <div className="flex items-end h-full">
+        <div className="h-[50vh]  px-12 flex flex-row gap-4">
+          <div className="h-full w-[1px] bg-gradient-to-b from-white to-white/0  relative">
+            <svg
+              viewBox="0 0 120 120"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              className="fill-white size-4 absolute -top-2 -left-2 "
+            >
+              <circle cx="60" cy="60" r="50" />
+            </svg>
+          </div>
+
+          <div className="flex flex-col gap-4 pt-6">
+            <h3 className="text-3xl font-semibold tracking-tighter text-white md:text-4xl lg:text-5xl ">
+              Front-End Developer
+            </h3>
+            <p className="tracking-tight text-white/80 text-lg lg:text-2xl lg:max-w-[35vw]">
+              I blend my experiences in UX/UI Design and customer-facing roles
+              to create meaningful and user-centric digital experiences.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
