a0eaa563d585b03cb10bd88083bd5d16a8312b82
MMadlos <michaelmadlos@gmail.com>
2024-05-07 16:55:00 +0200

    Add styles

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,10 +5,10 @@ export default function Hero() {
       <div class="circle-sm right"></div>
       <div class="circle-lg"></div>
 
-      <div className="flex items-end h-full">
-        <div className="h-[35vh]  px-12 flex flex-row gap-4 ">
+      <div className="flex justify-center items-end h-full">
+        <div className="h-[50vh]  px-12 flex flex-row gap-4 ">
           <div className="relative">
-            <div className="h-full w-[1px] bg-gradient-to-b from-white to-white/0"></div>
+            <div className="h-full w-[1px] bg-gradient-to-b from-white to-white/10"></div>
             <svg
               viewBox="0 0 120 120"
               version="1.1"
@@ -23,7 +23,7 @@ export default function Hero() {
             <h3 className="text-3xl font-semibold tracking-tighter text-white md:text-4xl lg:text-5xl ">
               Hi there! <br /> I'm a Front-End Developer
             </h3>
-            <p className="tracking-tight text-white/80 text-lg lg:text-2xl lg:max-w-[35vw]">
+            <p className="tracking-tight text-white/80 text-lg lg:text-2xl lg:max-w-[50%]">
               I blend my experiences in UX/UI Design and customer-facing roles
               to create meaningful and user-centric digital experiences.
             </p>
--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -4,50 +4,68 @@ export default function ProjectCard({ projectData }) {
   const { website, github } = links;
 
   return (
-    <article className="flex flex-col sm:flex-row gap-16 border border-[#5810A2]/20 hover:border-[#5810A2]/70 py-[24px] px-[40px] rounded-md bg-[#1D0D35]/40 hover:bg-[#1D0D35]/70 backdrop-blur group-hover/list:opacity-15 hover:!opacity-100 transition-opacity">
-      <picture>
-        <img
-          src={imgURL}
-          alt=""
-          className="rounded-sm sm:max-w-[250px] aspect-square object-cover object-right-bottom"
-        />
-      </picture>
+    <a href={website} className="group ">
+      <article className="flex flex-col sm:flex-row gap-16 border border-[#5810A2]/20 hover:border-[#5810A2]/70 py-[24px] px-[40px] rounded-md bg-[#5810A2]/10 hover:bg-[#1D0D35]/70 backdrop-blur group-hover/list:opacity-15 hover:!opacity-100 transition-opacity ">
+        <picture className="">
+          <img
+            src={imgURL}
+            alt=""
+            className="rounded-sm sm:max-w-[200px] aspect-square object-cover object-right-bottom "
+          />
+        </picture>
 
-      <div className="flex flex-col gap-10">
-        <ul className="flex gap-3 flex-wrap text-[#B080FF] text-xs">
-          {technologies.map((label, index) => {
-            return (
-              <li
-                key={index}
-                className="px-4 py-1 bg-[#450B7D]/50 rounded-full "
+        <div className="flex flex-col gap-10 justify-between">
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-4 items-center ">
+              <h2 className="font-medium text-white text-xl group-hover:underline group-hover:underline-offset-4 ">
+                {header}
+              </h2>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6 text-[#B080FF] opacity-0  -translate-x-10 group-hover:translate-x-0 group-hover:opacity-100 transition "
               >
-                {label}
-              </li>
-            );
-          })}
-        </ul>
+                <path
+                  fill-rule="evenodd"
+                  d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
 
-        <div className="flex flex-col gap-2">
-          <h2 className="font-bold text-white text-xl">{header}</h2>
+            <p className="font-medium text text-white/50">{description}</p>
+          </div>
 
-          <p className="font-medium text text-white/70">{description}</p>
+          <ul className="flex gap-3 flex-wrap text-[#B080FF] text-xs">
+            {technologies.map((label, index) => {
+              return (
+                <li
+                  key={index}
+                  className="px-4 py-1 bg-[#450B7D]/50 rounded-full "
+                >
+                  {label}
+                </li>
+              );
+            })}
+          </ul>
+
+          {/* <div className="flex flex-row gap-4 text-white">
+            <a
+              href={website}
+              className="bg-[#8214EF] px-4 py-2 rounded text-sm font-semibold hover:bg-[#450B7D] transition"
+            >
+              Live demo
+            </a>
+            <a
+              href={github}
+              className="px-4 py-2 rounded text-sm font-medium hover:underline "
+            >
+              Github
+            </a>
+          </div> */}
         </div>
-
-        <div className="flex flex-row gap-4 text-white">
-          <a
-            href={website}
-            className="bg-[#8214EF] px-4 py-2 rounded text-sm font-semibold hover:bg-[#450B7D] transition"
-          >
-            Live demo
-          </a>
-          <a
-            href={github}
-            className="px-4 py-2 rounded text-sm font-medium hover:underline "
-          >
-            Github
-          </a>
-        </div>
-      </div>
-    </article>
+      </article>
+    </a>
   );
 }
