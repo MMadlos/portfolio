76f5391fb13ac41e070d3b4cfec4a50f0883e5f4
MMadlos <michaelmadlos@gmail.com>
2024-05-07 19:27:15 +0200

    Update styles

--- src/components/projects/ProjectList.jsx
+++ src/components/projects/ProjectList.jsx
@@ -22,7 +22,7 @@ export default function ProjectList() {
             <path d="M5.26 17.242a.75.75 0 1 0-.897-1.203 5.243 5.243 0 0 0-2.05 5.022.75.75 0 0 0 .625.627 5.243 5.243 0 0 0 5.022-2.051.75.75 0 1 0-1.202-.897 3.744 3.744 0 0 1-3.008 1.51c0-1.23.592-2.323 1.51-3.008Z" />
           </svg>
 
-          <h1 className="text-lg font-medium text-white ">Projects</h1>
+          <h1 className="  text-[#B080FF] uppercase">Projects</h1>
         </div>
         <div className="flex flex-col gap-4 group/list relative">
           {PROJECTS_DATA.map((projectData, index) => {
