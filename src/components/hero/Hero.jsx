39039b4493e09fa60fbf7eb7c1c3569f28eb3477
MMadlos <michaelmadlos@gmail.com>
2024-05-07 19:01:01 +0200

    Adjust styles

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,24 +8,24 @@ export default function Hero() {
       <div className="flex justify-center items-end h-full">
         <div className="h-[50vh]  px-12 flex flex-row gap-4 ">
           <div className="relative">
-            <div className="h-full w-[1px] bg-gradient-to-b from-white to-white/10"></div>
+            <div className="h-full w-[1px] bg-gradient-to-b from-white to-#[1E0E2E] to-90%"></div>
             <svg
               viewBox="0 0 120 120"
               version="1.1"
               xmlns="http://www.w3.org/2000/svg"
-              className="fill-white size-4 absolute -top-2 -left-2 "
+              className="fill-white size-4 absolute -top-2 -left-2 animate-bounce shadow"
             >
               <circle cx="60" cy="60" r="50" />
             </svg>
           </div>
 
           <div className="flex flex-col gap-4 pt-6">
-            <h3 className="text-3xl font-semibold tracking-tighter text-white md:text-4xl lg:text-5xl ">
-              Hi there! <br /> I'm a Front-End Developer
+            <h3 className="text-3xl font-semibold tracking-tight text-white md:text-4xl lg:text-6xl ">
+              Front-End Developer
             </h3>
-            <p className="tracking-tight text-white/80 text-lg lg:text-2xl lg:max-w-[50%]">
-              I blend my experiences in UX/UI Design and customer-facing roles
-              to create meaningful and user-centric digital experiences.
+            <p className=" text-white/50 text-lg lg:text-lg lg:max-w-[60%]">
+              I blend my experience in UX/UI Design and customer-facing roles to
+              create meaningful and user-centric digital experiences.
             </p>
           </div>
         </div>
