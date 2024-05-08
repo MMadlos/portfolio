f0ee20d3dd9ee9c8655b2f08e1532df69586938e
MMadlos <michaelmadlos@gmail.com>
2024-05-08 12:37:53 +0200

    Adjust styles for xs screens

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,12 +1,12 @@
 export default function Hero() {
   return (
-    <section className="h-[100vh] relative overflow-hidden hero-bg">
+    <section className="h-[100vh] relative overflow-x-hidden hero-bg">
       <div class="circle-sm left"></div>
       <div class="circle-sm right"></div>
       <div class="circle-lg"></div>
 
       <div className="flex justify-center items-end h-full">
-        <div className="h-[70vh] flex flex-row gap-4 lg:max-w-[600px]">
+        <div className="gap-6 px-4 sm:px-0 h-[80vh] sm:h-[70vh] flex flex-row sm:gap-4 max-w-[600px]">
           <div className="relative">
             <div className="h-full w-[1px] bg-gradient-to-b from-white to-#[1E0E2E] to-100%"></div>
             <svg
@@ -19,31 +19,31 @@ export default function Hero() {
             </svg>
           </div>
 
-          <div className="flex flex-col gap-4 pt-6 pl-6">
-            <h4 className="text-white/50 font-semibold">Michael Madlos</h4>
+          <div className="flex flex-col gap-3 sm:gap-4 pt-6 sm:pl-6">
+            <h4 className="text-white/50 text-sm font-medium sm:text-base sm:font-semibold ">
+              Michael Madlos
+            </h4>
             <h3 className="text-3xl font-semibold tracking-tight text-white md:text-4xl">
               Front-End Developer
             </h3>
-            <p className=" text-white/50 text-base">
-              My background in UX/UI design and customer-centric roles has honed
-              my analytical and problemsolving skills. Managing technical
-              incidents and providing technical training have further enhanced
-              my ability to communicate complex concepts effectively to both
-              technical and non-technical users.
+            <p className="text-white/70 sm:text-white/50 text-base">
+              Background in UX/UI design and customer-centric roles. Experienced
+              in communicating complex concepts to both technical and
+              non-technical users. Problem solver and self-taught programmer.
             </p>
 
-            <div className="flex flex-row gap-4 mt-6">
+            <div className="flex flex-row gap-4 mt-6 ">
               <a
                 href="https://www.linkedin.com/in/michaelmadlos/"
                 target="_blank"
-                className="bg-[#8214EF] text-white py-2 px-4 rounded font-semibold min-w-[150px] hover:opacity-60 text-center"
+                className="bg-[#8214EF] text-white py-2 px-6 rounded font-semibold sm:min-w-[150px] hover:opacity-60 text-center"
               >
                 LinkedIn
               </a>
               <a
                 href="https://github.com/MMadlos"
                 target="_blank"
-                className="bg-black text-white py-2 px-4 rounded font-semibold min-w-[150px] hover:opacity-60 text-center"
+                className="bg-black text-white py-2 px-6 rounded font-semibold sm:min-w-[150px] hover:opacity-60 text-center"
               >
                 GitHub
               </a>
